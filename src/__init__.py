cc08a0367d2950e20a413c49727ae2adc67fb70a
omarhamza <omarhamza37@gmail.com>
2025-07-19 09:18:02 +0000

    feat: Remove the while loop
    Remove the while loop and rely on the service configuration for restarts in order to improve performance

--- src/__init__.py
+++ src/__init__.py
@@ -1,14 +1,11 @@
 import time
-from config import SLEEP_TIME
 from send_telegram_message import notify
 from decision import ShouldIBuyCrypto
         
 notify(f"✅ *Start running XGB V1.0!*")
 
-while True:
-    try:
-        ShouldIBuyCrypto()
-    except Exception as e:
-        notify(f"❌❌❌ Erreur globale : {e}")
-    print(f"⏳ Pause de {SLEEP_TIME} minutes avant le prochain cycle...\n")
-    time.sleep(SLEEP_TIME * 60)
+try:
+    ShouldIBuyCrypto()
+except Exception as e:
+    notify(f"❌❌❌ Erreur globale : {e}")
+print(f"⏳ Pause de {SLEEP_TIME} minutes avant le prochain cycle...\n")

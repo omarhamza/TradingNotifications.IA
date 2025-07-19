2923542d2b1280a58c47b91680e59430e4c93930
omarhamza <omarhamza37@gmail.com>
2025-07-19 21:01:33 +0000

    feat: remove start notification
    remove start notification in order to not spam telegram bot

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,7 @@
-import time
 from send_telegram_message import notify
 from decision import ShouldIBuyCrypto
-        
-notify(f"✅ *Start running XGB V1.0!*")
 
 try:
     ShouldIBuyCrypto()
 except Exception as e:
     notify(f"❌❌❌ Erreur globale : {e}")
-print(f"⏳ Pause de {SLEEP_TIME} minutes avant le prochain cycle...\n")
